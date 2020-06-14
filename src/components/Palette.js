8798bbe56bc212a0ed2584a74cdb073565f56d41
fouad97 <loukrichi21@gmail.com>
2020-06-14 17:31:50 +0100

    add select and Material UI

--- src/components/Palette.js
+++ src/components/Palette.js
@@ -6,7 +6,7 @@ import "./Palette.css";
 export default class Palette extends Component {
 	constructor(props) {
 		super(props);
-		this.state = { level: 500 };
+		this.state = { level: 500, format: "hex" };
 	}
 
 	changeLevel = (level) => {
@@ -14,17 +14,29 @@ export default class Palette extends Component {
 		console.log(level);
 	};
 
+	changeFormat = (e) => {
+		this.setState({ format: e });
+	};
+
 	render() {
 		const { colors } = this.props.palette;
 		const { level } = this.state;
 		const colorBoxes = colors[level].map((color) => {
 			return (
-				<ColorBox background={color.hex} name={color.name} key={color.name} />
+				<ColorBox
+					background={color[this.state.format]}
+					name={color.name}
+					key={color.name}
+				/>
 			);
 		});
 		return (
 			<div className="Palette">
-				<Navbar level={level} changeLevel={this.changeLevel} />
+				<Navbar
+					level={level}
+					changeLevel={this.changeLevel}
+					handleChange={this.changeFormat}
+				/>
 				<div className="Palette-color">{colorBoxes}</div>
 			</div>
 		);
